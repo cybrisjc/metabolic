ef001d7ae7608b2571b1720809d4653d94414b18
cybrisjc <cybrisjc@gmail.com>
2025-07-15 11:30:33 -0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: ['./index.html', './dist/index.html'],
+  theme: { extend: {} },
   plugins: [],
 }
-
